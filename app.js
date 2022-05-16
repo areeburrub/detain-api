c22f438da96963f113f693e9105a518a54883f2d
areeburrub <areeburrub@gmail.com>
2022-05-16 22:09:56 +0530

    init

--- app.js
+++ app.js
@@ -7,109 +7,122 @@ const app = express()
 app.get('/attendance', async (req, res) => {
 
     const ad_number = req.query.adno
+    const pswd = req.query.pswd || "GCET123"
     if (!ad_number) {
-        res.status(400).send({error: "Admission number is not found!"})
-
+      res.status(400).send({ error: "Admission number is not found!" });
     } else {
-        const browser = await puppeteer.launch();
-        const page = await browser.newPage();
+      const browser = await puppeteer.launch();
+      const page = await browser.newPage();
 
-        await page.goto("https://gu.icloudems.com")
+      await page.goto("https://gu.icloudems.com");
 
-        await page.waitForSelector("#useriid")
-        await page.focus('#useriid')
-        await page.keyboard.type(ad_number)
+      await page.waitForSelector("#useriid");
+      await page.focus("#useriid");
+      await page.keyboard.type(ad_number);
 
+      await page.waitForSelector("#actlpass");
+      await page.focus("#actlpass");
+      await page.keyboard.type(pswd);
+      await page.waitForTimeout(1000);
 
-        await page.waitForSelector("#actlpass")
-        await page.focus('#actlpass')
-        await page.keyboard.type('GCET123')
+      await (await page.$("#psslogin")).press("Enter"); // Enter Key
+    console.log("Logged in");   
+    await page.waitForTimeout(2000);
+    
+    if (
+        page
+        .url()
+        .includes(
+            "errormessage=Invalid+Username+or+Password.Please+try+again."
+            )
+            )
+            res.status(400).send("Invalid Login Credentials");
+            else {
+                await page.goto(
+                    "https://gu.icloudems.com/corecampus/student/attendance/subwise_attendace_new.php"
+                    );
+                    //
+                    console.log("in attendance");
+                    await page.waitForTimeout(2000);
+                    
+                    //await page.click("#select2-acadyear-container")
+                    //await (await page.$('.select2-selection__rendered')).press('Enter'); // Enter Key
+                    //await (await page.$('#select2-acadyear-result-npqk-2021-2022')).press('Enter'); // Enter Key
+                    
+                    await page.select("#acadyear", "2021-2022");
+                    await page.waitForTimeout(2000);
+                    await page.select("#classid", "2816");
+                    await page.waitForTimeout(1000);
+                    await (await page.$("#getattendance")).press("Enter"); // Enter Key
+                    console.log("fetched attendace");
+                    
+                    await page.waitForTimeout(2000);
+                    console.log("fetching table");
 
-        await page.waitForTimeout(1000);
+        const result = await page.$$eval(".table tr", (rows) => {
+          return Array.from(rows, (row) => {
+            const columns = row.querySelectorAll("td");
+            return Array.from(columns, (column) => column.textContent);
+          });
+        });
 
-        await (await page.$('#psslogin')).press('Enter'); // Enter Key
+        let final = [];
 
-        await page.waitForTimeout(2000);
+        let final_percentage = {
+          total_classes: "",
+          percentage: "",
+        };
 
-        if (page.url().includes("errormessage=Invalid+Username+or+Password.Please+try+again."))
-            res.status(400).send("Invalid Login Credentials")
-        else {
-            await page.goto('https://gu.icloudems.com/corecampus/student/attendance/subwise_attendace_new.php')
-            //
-            await page.waitForTimeout(2000);
+        //get photo and name as well
 
-            //await page.click("#select2-acadyear-container")
-            //await (await page.$('.select2-selection__rendered')).press('Enter'); // Enter Key
-            //await (await page.$('#select2-acadyear-result-npqk-2021-2022')).press('Enter'); // Enter Key
+        //const img = await page.$$eval('.rounded-circle.rounded.img-rounded.image-cropper > img[src]', imgs => imgs.map(img => img.getAttribute('src')));
 
-            await page.select('#acadyear', '2021-2022')
-            await page.waitForTimeout(2000)
-            await page.select('#classid', '2816')
-            await page.waitForTimeout(1000)
-            await (await page.$('#getattendance')).press('Enter'); // Enter Key
+        const images = await page.$$eval("img", (anchors) =>
+          [].map.call(anchors, (img) => img.src)
+        );
 
+        const dp =
+          images[
+            images.findIndex((element) => element.includes("/student_profile"))
+          ];
 
-            await page.waitForTimeout(2000)
+        const name = await page.evaluate(() => {
+          const elements = document.getElementsByClassName(
+            "d-none d-lg-inline-block mr-2"
+          );
+          return Array.from(elements).map((element) => element.innerText);
+        });
 
-
-            const result = await page.$$eval('.table tr', rows => {
-                return Array.from(rows, row => {
-                    const columns = row.querySelectorAll('td');
-                    return Array.from(columns, column => column.textContent);
-                });
-            });
-
-            let final = [];
-
-            let final_percentage = {
-                'total_classes': '',
-                'percentage': '',
-            }
-
-            //get photo and name as well
-
-            //const img = await page.$$eval('.rounded-circle.rounded.img-rounded.image-cropper > img[src]', imgs => imgs.map(img => img.getAttribute('src')));
-
-            const images = await page.$$eval('img', anchors => [].map.call(anchors, img => img.src));
-
-            const dp = images[images.findIndex(element => element.includes("/student_profile"))]
-
-            const name = await page.evaluate(() => {
-                const elements = document.getElementsByClassName('d-none d-lg-inline-block mr-2');
-                return Array.from(elements).map(element => element.innerText);
-            })
-
-            for (let i = 0; i < result.length; i++) {
-                let data = {
-                    'serial_number': '',
-                    'name': '',
-                    'subject_code': '',
-                    'code': '',
-                    'classes': '',
-                    'percentage': ''
-                }
-                for (let j = 0; j < result[i].length; j++) {
-                    if (i === (result.length - 1)) {
-                        if (j !== 0)
-                            final_percentage[Object.keys(final_percentage)[j - 1]] = result[i][j];
-                    } else
-                        data[Object.keys(data)[j]] = result[i][j];
-                }
-                if (!isEmpty(final_percentage)) {
-                    final.push(final_percentage)
-                }
-                if (!isEmpty(data)) {
-                    final.push(data)
-                }
-
-            }
-
-            let metadata = {name, dp}
-            final.push({metadata})
-
-            res.status(200).send({response: final})
+        for (let i = 0; i < result.length; i++) {
+          let data = {
+            serial_number: "",
+            name: "",
+            subject_code: "",
+            code: "",
+            classes: "",
+            percentage: "",
+          };
+          for (let j = 0; j < result[i].length; j++) {
+            if (i === result.length - 1) {
+              if (j !== 0)
+                final_percentage[Object.keys(final_percentage)[j - 1]] =
+                  result[i][j];
+            } else data[Object.keys(data)[j]] = result[i][j];
+          }
+          if (!isEmpty(final_percentage)) {
+            final.push(final_percentage);
+          }
+          if (!isEmpty(data)) {
+            final.push(data);
+          }
         }
-        await browser.close()
+
+        let metadata = { name, dp };
+        final.push({ metadata });
+
+        res.status(200).send({ response: final });
+      }
+      await browser.close();
     }
 })
 
